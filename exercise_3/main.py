c90a6a3945b0c08bf4bcdd51690fa1eca8dae4fa
rusov63 <rusov63@yahoo.com>
2023-04-27 22:52:40 +0400

    add main.py

---
+++ exercise_3/main.py
@@ -0,0 +1,33 @@
+import json
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+
+hostName = "localhost"
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write('<html><head><meta charset="utf-8">'.encode())
+        self.wfile.write('<title>Простой HTTP-сервер.</title></head>'.encode())
+        self.wfile.write('<body>“Hello, World wide web!”.</body></html>'.encode())
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
+
